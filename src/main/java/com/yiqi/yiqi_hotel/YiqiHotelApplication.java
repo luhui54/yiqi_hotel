ebdfb8cf48c50f2d9c783fe6a5dbe4bfc12a83a5
lh <lh1208008817@qq.com>
2025-06-11 21:50:27 +0800

    Fix: Configure component scan to include api_backend module

--- src/main/java/com/yiqi/yiqi_hotel/YiqiHotelApplication.java
+++ src/main/java/com/yiqi/yiqi_hotel/YiqiHotelApplication.java
@@ -3,7 +3,7 @@ package com.yiqi.yiqi_hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.yiqi.yiqi_hotel", "com.yiqi.api_backend"})
 public class YiqiHotelApplication {
 
     public static void main(String[] args) {
